26b70c1424c32680ca5279d9f1f9fe4a8d4732e3
zeldekk <blazej.glowiak@gmail.com>
2024-11-27 21:33:07 +0100

    added reply logging

--- events/messageDelete.js
+++ events/messageDelete.js
@@ -1,8 +1,15 @@
-module.exports = function messageDelete(message) {
+const { EmbedBuilder } = require('discord.js');
+
+module.exports = async function messageDelete(message) {
     if (message.author.bot || message.author.id === '952296941339934720' || message.author.id === '544594311627538443' || message.author.id === '711947999831523349') return;
     const embed = new EmbedBuilder()
-    .setAuthor({name: message.author.username, iconURL: message.author.avatarURL()})
-    .setDescription(message.content || 'null')
-    .setColor('Random')
+        .setAuthor({name: message.author.username, iconURL: message.author.avatarURL()})
+        if (message.reference) {
+            const repliedMessage = await message.channel.messages.fetch(message.reference.messageId);
+            embed.setDescription('> ' + repliedMessage.content + '\n' + message.content || 'null');
+        } else {
+            embed.setDescription(message.content || 'null')
+        }
+        embed.setColor('Random')
     message.channel.send({embeds: [embed]});
 }
--- events/messageUpdate.js
+++ events/messageUpdate.js
@@ -1,4 +1,6 @@
-module.exports = function messageUpdate(oldMessage, newMessage) {
+const { EmbedBuilder } = require('discord.js');
+
+module.exports = async function messageUpdate(oldMessage, newMessage) {
     if (oldMessage.content === newMessage.content) return;
     if (oldMessage.author.bot || oldMessage.author.id === '952296941339934720' || oldMessage.author.id === '544594311627538443' || oldMessage.author.id === '711947999831523349') return;
 
@@ -7,10 +9,19 @@ module.exports = function messageUpdate(oldMessage, newMessage) {
             name: newMessage.author.username,
             iconURL: newMessage.author.avatarURL()
         })
-        .addFields(
-            { name: 'Przed edytowaniem', value: oldMessage.content || 'null', inline: false },
-            { name: 'Po edytowaniu', value: newMessage.content || 'null', inline: false }
-        )
-        .setColor('Random')
+        if (oldMessage.reference) {
+            const repliedMessage = await oldMessage.channel.messages.fetch(oldMessage.reference.messageId);
+            embed.addFields(
+                { name: 'Przed edytowaniem', value: '> ' + repliedMessage.content + '\n' + oldMessage.content || 'null', inline: false },
+                { name: 'Po edytowaniu', value: newMessage.content || 'null', inline: false }
+            )
+        } else {
+            embed.addFields(
+                { name: 'Przed edytowaniem', value: oldMessage.content || 'null', inline: false },
+                { name: 'Po edytowaniu', value: newMessage.content || 'null', inline: false }
+            )
+        }
+        
+        embed.setColor('Random')
     newMessage.channel.send({ embeds: [embed] });
 }
