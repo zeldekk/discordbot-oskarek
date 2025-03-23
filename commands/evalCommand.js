b2b3c5111ddcc9ced2a4d70e2da8c957a0696150
zeldekk <blazej.glowiak@gmail.com>
2025-03-23 01:10:15 +0100

    added basic flags for eval cmd

--- commands/evalCommand.js
+++ commands/evalCommand.js
@@ -2,13 +2,27 @@ const { EmbedBuilder } = require('discord.js');
 
 module.exports = async function evalCommand(message) {
     if (message.author.id !== '952296941339934720') return;
+
     const code = message.content.slice(6);
+
+    let flags = [];
+
+    const matches = message.content.match(/-(\w+)/g) || [];
+
+    if (matches) {
+        flags.push(...matches.map(flag => flag[1]));
+    }
     try {
         let result = await eval(code);
-
-        const evalEmbed = new EmbedBuilder().setTitle('eval').addFields({name: 'kod',value: `\`\`\`js\n${code}\`\`\``},{name: 'result', value: `\`\`\`${result.toString()}\`\`\``}).setColor('Random')
-        message.channel.send({embeds: [evalEmbed]});
+        if (!flags.includes("s")) {
+            const evalEmbed = new EmbedBuilder().setTitle('eval').addFields({name: 'kod',value: `\`\`\`js\n${code}\`\`\``},{name: 'result', value: `\`\`\`${result.toString()}\`\`\``}).setColor('Random')
+            message.channel.send({embeds: [evalEmbed]});
+        }
     } catch (error) {
-        message.channel.send(`\`\`\`js\n${error}\n\`\`\``);
+        console.log(error);
+    }
+
+    if (flags.includes('d')) {
+        message.delete();
     }
 }
